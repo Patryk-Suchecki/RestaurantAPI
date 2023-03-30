99dd18393204fb49d59df4686f9fdd63b2375e48
BuildTools <unconfigured@null.spigotmc.org>
2023-03-30 09:15:31 +0200

    Small fixes from previous commit

--- RestaurantAPI/Controllers/RestaurantController.cs
+++ RestaurantAPI/Controllers/RestaurantController.cs
@@ -25,20 +25,19 @@ namespace RestaurantAPI.Controllers
         [HttpDelete("{id}")]
         public ActionResult Delete([FromRoute]int id)
         {
-            _restaurantService.Delete(id, User);
+            _restaurantService.Delete(id);
             return NoContent();
         }
         [HttpPut("{id}")]
         public ActionResult Update([FromRoute] int id, [FromBody] UpdateRestaurantDto dto)
         {
-            _restaurantService.Update(id, dto, User);
+            _restaurantService.Update(id, dto);
             return Ok();
         }
         [HttpPost]
         public ActionResult CreateRestaurant([FromBody]CreateRestaurantDto dto)
         {
-            var userId = int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value);
-            var id = _restaurantService.Create(dto, userId);
+            var id = _restaurantService.Create(dto);
 
             return Created($"/api/restaurant/{id}", null);
         }
