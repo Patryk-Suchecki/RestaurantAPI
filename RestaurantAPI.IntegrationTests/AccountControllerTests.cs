6e56b629a4ac60045acee2081e4f27fc1c736924
BuildTools <unconfigured@null.spigotmc.org>
2023-05-03 17:25:51 +0200

    Account service tests added

---
+++ RestaurantAPI.IntegrationTests/AccountControllerTests.cs
@@ -0,0 +1,96 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using RestaurantAPI.entity;
+using RestaurantAPI.IntegrationTests.Helpers;
+using RestaurantAPI.Models;
+using RestaurantAPI.Services;
+
+namespace RestaurantAPI.IntegrationTests
+{
+    public class AccountControllerTests: IClassFixture<WebApplicationFactory<Program>>
+    {
+        private HttpClient _client;
+        private Mock<IAccountService> _accountServiceMock = new Mock<IAccountService>();
+
+        public AccountControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        var dbContextOptions = services.SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<RestaurantDbContext>));
+                        services.Remove(dbContextOptions);
+
+                        services.AddSingleton<IAccountService>(_accountServiceMock.Object);
+
+                        services.AddDbContext<RestaurantDbContext>(options => options.UseInMemoryDatabase("RestaurantDb"));
+                    });
+                }).CreateClient();
+        }
+
+        [Fact]
+        public async Task Login_ForRegisteredUser_ReturnsOk()
+        {
+            // arrange
+            _accountServiceMock
+                .Setup(e => e.GenerateJwt(It.IsAny<LoginDto>()))
+                .Returns("jwt");
+
+            var loginDto = new LoginDto()
+            {
+                Email = "test@test.com",
+                Password = "123456",
+            };
+
+            var httpContent = loginDto.ToJsonHttpContent();
+
+            // act
+            var response = await _client.PostAsync("/api/account/login", httpContent);
+
+            // assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task RegisterUser_ForValidModel_ReturnsOk()
+        {
+            // arrange
+            var registerUser = new RegisterUserDto()
+            {
+                Email = "test@test.com",
+                Password = "123456",
+                ConfirmPassword = "123456",
+            };
+
+            var httpContent = registerUser.ToJsonHttpContent();
+
+            // act
+            var response = await _client.PostAsync("/api/account/register", httpContent);
+
+            // assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task RegisterUser_ForInvalidModel_ReturnsBadRequest()
+        {
+            // arrange
+            var registerUser = new RegisterUserDto()
+            {
+                Password = "123456",
+                ConfirmPassword = "123",
+            };
+
+            var httpContent = registerUser.ToJsonHttpContent();
+
+            // act
+            var response = await _client.PostAsync("/api/account/register", httpContent);
+
+            // assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+        }
+    }
+}
--- RestaurantAPI.IntegrationTests/RestaurantControllerTests.cs
+++ RestaurantAPI.IntegrationTests/RestaurantControllerTests.cs
@@ -2,6 +2,11 @@
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using RestaurantAPI.entity;
+using RestaurantAPI.Models;
+using Newtonsoft.Json;
+using System.Text;
+using Microsoft.AspNetCore.Authorization.Policy;
+using RestaurantAPI.IntegrationTests.Helpers;
 
 namespace RestaurantAPI.IntegrationTests
 {
@@ -9,21 +14,38 @@ namespace RestaurantAPI.IntegrationTests
     public class RestaurantControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
         private HttpClient _client;
+        private WebApplicationFactory<Program> _factory;
 
         public RestaurantControllerTests(WebApplicationFactory<Program> factory)
         {
-            _client = factory.WithWebHostBuilder(builder =>
+            _factory = factory
+                .WithWebHostBuilder(builder =>
             {
                 builder.ConfigureServices(services =>
                 {
                     var dbContextOptions = services.SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<RestaurantDbContext>));
                     services.Remove(dbContextOptions);
 
+                    services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+                    services.AddMvc(option => option.Filters.Add(new FakeUserFilter()));
+
                     services.AddDbContext<RestaurantDbContext>(options => options.UseInMemoryDatabase("RestaurantDb"));
                 });
-            })
-                .CreateClient();
+            });
+
+            _client = _factory.CreateClient();
         }
+
+        private void SeedRestaurant(Restaurant restaurant)
+        {
+            var scopeFactory = _factory.Services.GetService<IServiceScopeFactory>();
+            using var scope = scopeFactory.CreateScope();
+            var _dbContext = scope.ServiceProvider.GetService<RestaurantDbContext>();
+
+            _dbContext.Restaurants.Add(restaurant);
+            _dbContext.SaveChanges();
+        }
+
         [Theory]
         [InlineData("PageSize=10&PageNumber=1")]
         [InlineData("PageSize=15&PageNumber=2")]
@@ -37,6 +59,97 @@ namespace RestaurantAPI.IntegrationTests
             // assert
 
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Delete_ForNonRestaurantOwner_ReturnsForbidden()
+        {
+            // arrange
+            var restaurant = new Restaurant()
+            {
+                CreatedById = 900,
+                Name= "Test",
+            };
+
+            SeedRestaurant(restaurant);
+
+            // act
+            var response = await _client.DeleteAsync("/api/restaurant/" + restaurant.Id);
+
+            // assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.Forbidden);
+        }
+
+        [Fact]
+        public async Task Delete_ForRestaurantOwner_ReturnsNoContent()
+        {
+            // arrange
+            var restaurant = new Restaurant()
+            {
+                CreatedById = 1,
+                Name = "Test",
+            };
+
+            SeedRestaurant(restaurant);
+
+            // act
+            var response = await _client.DeleteAsync("/api/restaurant/" + restaurant.Id);
+
+            // assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.NoContent);
+
+        }
+
+        [Fact]
+        public async Task Delete_ForNonExistingRestaurant_ReturnsNotFound()
+        {
+            // act
+            var response = await _client.DeleteAsync("/api/restaurant/987");
+
+            // assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+
+        }
+        [Fact]
+        public async Task CreateRestaourant_WithValidModel_ReturnsCreatedStatus()
+        {
+            // arrange
+            var model = new CreateRestaurantDto()
+            {
+                Name = "Test",
+                City = "Kraków",
+                Street = "Długa 5"
+            };
+
+            var httpContent = model.ToJsonHttpContent();
+
+            // act
+            var response = await _client.PostAsync("/api/restaurant",httpContent);
+
+            // arrange
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+            response.Headers.Location.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task CreateRestaurant_WithInvalidModel_ReturnsBadRequest()
+        {
+            // arrange
+            var model = new CreateRestaurantDto()
+            {
+                ContactEmail = "Test",
+                Description = "Test",
+                ContactNumber = "Test",
+            };
+
+            var httpContent = model.ToJsonHttpContent();
+
+            // act
+            var response = await _client.PostAsync("/api/restaurant", httpContent);
+
+            // arrange
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
         }
 
         [Theory]
