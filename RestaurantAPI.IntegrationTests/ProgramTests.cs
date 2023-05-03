280114b050febbd78962a38aa6e0ff68f3922973
BuildTools <unconfigured@null.spigotmc.org>
2023-05-03 18:06:29 +0200

    Testing dependency registration

---
+++ RestaurantAPI.IntegrationTests/ProgramTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using RestaurantAPI.Controllers;
+
+namespace RestaurantAPI.IntegrationTests
+{
+    public class ProgramTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private List<Type> _controllerTypes;
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public ProgramTests(WebApplicationFactory<Program> factory)
+        {
+            _controllerTypes = typeof(Program)
+                .Assembly
+                .GetTypes()
+                .Where(t => t.IsSubclassOf(typeof(ControllerBase)))
+                .ToList();
+
+            _factory = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    _controllerTypes.ForEach(c => services.AddScoped(c));
+                });
+            });
+        }
+        [Fact]
+        public void ConfigureServices_ForControllers_RegisterAllDependencies()
+        {
+            var scopeFactory = _factory.Services.GetService<IServiceScopeFactory>();
+            using var scope = scopeFactory.CreateScope();
+
+            // assert
+            _controllerTypes.ForEach(t =>
+            {
+                var controller = scope.ServiceProvider.GetService(t);
+                controller.Should().NotBeNull();
+            });
+            
+            
+        }
+    }
+}
